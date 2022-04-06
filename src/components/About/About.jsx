c0a0557aeea2b4c2ddb7168bfaf8a362ea537266
Alex Lomeyko <raznoglaz1y@gmail.com>
2022-04-06 20:30:18 +0300

    ADD: Experience section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.png";
+import { FaAward, FaFolderOpen, FaUsers } from "react-icons/fa";
 
 const About = () => {
   return (
-    <section id="about">About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FaUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>12+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <FaFolderOpen className="about__icon" />
+              <h5>Projects</h5>
+              <small>17+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et
+            reiciendis quas enim doloremque soluta quasi assumenda ut id natus,
+            autem nemo modi laborum neque reprehenderit ex praesentium vel earum
+            commodi.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
