ec1a07c5a422227a44a5c1661e87846cb91696d0
Alex Lomeyko <raznoglaz1y@gmail.com>
2022-04-06 17:02:57 +0300

    ADD: Floating Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import "./nav.css";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineBook,
+  AiOutlineMessage,
+} from "react-icons/ai";
+import { RiServiceLine } from "react-icons/ri";
+import { useState } from "react";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav>
+      <a
+        title="Home"
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        title="About Me"
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        title="My Experience"
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <AiOutlineBook />
+      </a>
+      <a
+        title="Services"
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        title="Contacts"
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMessage />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
