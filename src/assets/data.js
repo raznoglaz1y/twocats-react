4e142ae92820b5b1144cf2f659b8fd6684c7bf18
Alex Lomeyko <raznoglaz1y@gmail.com>
2022-04-07 11:35:38 +0300

    ADD: Portfolio section

---
+++ src/assets/data.js
@@ -0,0 +1,47 @@
+import IMG1 from "./portfolio1.jpg"; 
+import IMG2 from "./portfolio2.jpg";
+import IMG3 from "./portfolio3.jpg";
+import IMG4 from "./portfolio4.jpg";
+import IMG5 from "./portfolio5.png";
+import IMG6 from "./portfolio6.jpg";
+
+export const portfolioItems = [
+  {
+    id: 1,
+    img: IMG1,
+    title: "Portfolio Item Title",
+    github: "https://github.com/",
+    demo: "https://github.com"
+  },
+  {
+    id: 2,
+    img: IMG2,
+    title: "Portfolio Item Title",
+    github: "https://github.com/",
+    demo: "https://github.com"
+  },  {
+    id: 3,
+    img: IMG3,
+    title: "Portfolio Item Title",
+    github: "https://github.com/",
+    demo: "https://github.com"
+  },  {
+    id: 4,
+    img: IMG4,
+    title: "Portfolio Item Title",
+    github: "https://github.com/",
+    demo: "https://github.com"
+  },  {
+    id: 5,
+    img: IMG5,
+    title: "Portfolio Item Title",
+    github: "https://github.com/",
+    demo: "https://github.com"
+  },  {
+    id: 6,
+    img: IMG6,
+    title: "Portfolio Item Title",
+    github: "https://github.com/",
+    demo: "https://github.com"
+  },
+];
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import { portfolioItems } from "../../assets/data";
 
 const Portfolio = () => {
   return (
-    <section>Portfolio</section>
-  )
-}
+    <section>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {portfolioItems.map(({ id, img, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={img} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
