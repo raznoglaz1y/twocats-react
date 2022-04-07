9fc0b6bc5d7eadb2f7bbc3f585346e47cffb03ee
Alex Lomeyko <raznoglaz1y@gmail.com>
2022-04-07 17:12:36 +0300

    ADD: Testimonials section

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,9 +1,14 @@
-import IMG1 from "./portfolio1.jpg"; 
+import IMG1 from "./portfolio1.jpg";
 import IMG2 from "./portfolio2.jpg";
 import IMG3 from "./portfolio3.jpg";
 import IMG4 from "./portfolio4.jpg";
 import IMG5 from "./portfolio5.png";
 import IMG6 from "./portfolio6.jpg";
+
+import AV1 from "./avatar1.jpg";
+import AV2 from "./avatar2.jpg";
+import AV3 from "./avatar3.jpg";
+import AV4 from "./avatar4.jpg";
 
 export const portfolioItems = [
   {
@@ -11,37 +16,72 @@ export const portfolioItems = [
     img: IMG1,
     title: "Portfolio Item Title",
     github: "https://github.com/",
-    demo: "https://github.com"
+    demo: "https://github.com",
   },
   {
     id: 2,
     img: IMG2,
     title: "Portfolio Item Title",
     github: "https://github.com/",
-    demo: "https://github.com"
-  },  {
+    demo: "https://github.com",
+  },
+  {
     id: 3,
     img: IMG3,
     title: "Portfolio Item Title",
     github: "https://github.com/",
-    demo: "https://github.com"
-  },  {
+    demo: "https://github.com",
+  },
+  {
     id: 4,
     img: IMG4,
     title: "Portfolio Item Title",
     github: "https://github.com/",
-    demo: "https://github.com"
-  },  {
+    demo: "https://github.com",
+  },
+  {
     id: 5,
     img: IMG5,
     title: "Portfolio Item Title",
     github: "https://github.com/",
-    demo: "https://github.com"
-  },  {
+    demo: "https://github.com",
+  },
+  {
     id: 6,
     img: IMG6,
     title: "Portfolio Item Title",
     github: "https://github.com/",
-    demo: "https://github.com"
+    demo: "https://github.com",
+  },
+];
+
+export const testimonialsData = [
+  {
+    id: 1,
+    avatar: AV1,
+    name: "Jane Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eaque et, at aspernatur modi dicta dolores eligendi? Culpa distinctio illo earum numquam neque, porro excepturi maiores cupiditate quibusdam, quasi omnis?",
+  },
+  {
+    id: 2,
+    avatar: AV2,
+    name: "Jane Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eaque et, at aspernatur modi dicta dolores eligendi? Culpa distinctio illo earum numquam neque, porro excepturi maiores cupiditate quibusdam, quasi omnis?",
+  },
+  {
+    id: 3,
+    avatar: AV3,
+    name: "Jane Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eaque et, at aspernatur modi dicta dolores eligendi? Culpa distinctio illo earum numquam neque, porro excepturi maiores cupiditate quibusdam, quasi omnis?",
+  },
+  {
+    id: 4,
+    avatar: AV4,
+    name: "Jane Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eaque et, at aspernatur modi dicta dolores eligendi? Culpa distinctio illo earum numquam neque, porro excepturi maiores cupiditate quibusdam, quasi omnis?",
   },
 ];
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import { testimonialsData } from "../../assets/data";
+
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
   return (
-    <section>Testimonials</section>
-  )
-}
+    <section>
+      <h5>Reviews From Clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        className="container testimonials__container"
+      >
+        {testimonialsData.map(({ id, avatar, name, review }) => {
+          return (
+            <SwiperSlide key={id} className="article testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
